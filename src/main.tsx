bae4786819ddb512eeb22d4cf0efa331a4cd0a0e
psw3077 <miso3077@naver.com>
2026-07-26 23:56:52 +0900

    Route admin query to admin dashboard

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import AdminDashboard from "./AdminDashboard";
 import "./styles.css";
 import "./v2.css";
 
+const isAdminPage = new URLSearchParams(window.location.search).get("admin") === "1";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    {isAdminPage ? <AdminDashboard /> : <App />}
   </React.StrictMode>,
 );
