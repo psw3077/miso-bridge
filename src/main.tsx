43bab7c0c53b9a9e1864f51005be25abaf9e3e88
psw3077 <miso3077@naver.com>
2026-08-07 23:03:53 +0900

    Route contract mode to MISO ONE contract center

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import MisoAdmin from "./MisoAdmin";
+import ContractCenter from "./contracts/ContractCenter";
 import "./styles.css";
 import "./v2.css";
+import "./contracts/contracts.css";
 
-const isAdminPage = new URLSearchParams(window.location.search).get("admin") === "1";
+const params = new URLSearchParams(window.location.search);
+const isAdminPage = params.get("admin") === "1";
+const isContractPage = params.get("contracts") === "1" || window.location.pathname.startsWith("/contracts");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {isAdminPage ? <MisoAdmin /> : <App />}
+    {isContractPage ? <ContractCenter /> : isAdminPage ? <MisoAdmin /> : <App />}
   </React.StrictMode>,
 );
