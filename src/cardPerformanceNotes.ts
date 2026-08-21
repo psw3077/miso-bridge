05546f9167cfb701ad426875efaf25317a58ea00
psw3077 <miso3077@naver.com>
2026-08-21 19:52:03 +0900

    Document MISO CARD performance ranking rules

---
+++ src/cardPerformanceNotes.ts
@@ -0,0 +1,5 @@
+export const MISO_CARD_PERFORMANCE_RULES = {
+  ranking: ['approved', 'applications', 'views'],
+  periods: [7, 30, 0],
+  description: '직원별 전자명함 성과는 승인 건수, 신규거래 신청 건수, 조회수 순으로 비교합니다.'
+} as const;
